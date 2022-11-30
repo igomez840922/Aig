14a32f45cbad9e846b69893a0c684c7bea583b82
yulier.garcia <yulier.garcia85@gmail.com>
2022-11-30 17:57:18 -0500

    fix

--- Aig.Farmacoterapia/Aig.Farmacoterapia.Public/Program.cs
+++ Aig.Farmacoterapia/Aig.Farmacoterapia.Public/Program.cs
@@ -32,7 +32,7 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Oops!");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
