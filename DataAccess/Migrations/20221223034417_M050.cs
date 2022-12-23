4ce2f1406ac829b385658a66a1d2ae51982ebf26
igomez2 <igomez@soaint.com>
2022-12-22 23:04:50 -0500

    update

---
+++ DataAccess/Migrations/20221223034417_M050.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class M050 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DosisEsavi",
+                table: "FMV_Esavi");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "VacunaComercial",
+                table: "FMV_Esavi",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DosisPresenta",
+                table: "FMV_Esavi",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DosisPresenta",
+                table: "FMV_Esavi");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "VacunaComercial",
+                table: "FMV_Esavi",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DosisEsavi",
+                table: "FMV_Esavi",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true);
+        }
+    }
+}
