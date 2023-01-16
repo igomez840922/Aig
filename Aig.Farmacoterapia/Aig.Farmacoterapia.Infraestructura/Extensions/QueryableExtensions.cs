a28b67205f87ae8f1cfae392f0352670b11e6064
yulier.garcia <yulier.garcia85@gmail.com>
2023-01-16 11:55:18 -0500

    fix

--- Aig.Farmacoterapia/Aig.Farmacoterapia.Infraestructura/Extensions/QueryableExtensions.cs
+++ Aig.Farmacoterapia/Aig.Farmacoterapia.Infraestructura/Extensions/QueryableExtensions.cs
@@ -11,12 +11,14 @@ namespace Aig.Farmacoterapia.Infrastructure.Extensions
     {
         public static async Task<PaginatedResult<T>> PaginatedByAsync<T>(this IQueryable<T> source, int pageNumber, int pageSize) where T : class
         {
+            var items = new List<T>();
             if (source == null) throw new Exception();
             pageNumber = pageNumber == 0 ? 1 : pageNumber;
             pageSize = pageSize == 0 ? 10 : pageSize;
             int count = await source.CountAsync();
             pageNumber = pageNumber <= 0 ? 1 : pageNumber;
-            List<T> items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+            if ((pageNumber - 1) * pageSize > count) items = await source.Take(pageSize).ToListAsync();
+            else items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
             return PaginatedResult<T>.Success(items, count, pageNumber, pageSize);
         }
 
