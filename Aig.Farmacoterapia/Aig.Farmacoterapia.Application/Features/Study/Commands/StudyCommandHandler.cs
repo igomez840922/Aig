a454bddb6e73d387fae5f3497162de85c069469a
yulier.garcia <yulier.garcia85@gmail.com>
2023-03-13 12:04:52 -0400

    updated

--- Aig.Farmacoterapia/Aig.Farmacoterapia.Application/Features/Study/Commands/StudyCommandHandler.cs
+++ Aig.Farmacoterapia/Aig.Farmacoterapia.Application/Features/Study/Commands/StudyCommandHandler.cs
@@ -158,52 +158,6 @@ namespace Aig.Farmacoterapia.Application.Features.Study.Commands
             return answer;
         }
 
-        //public string Codigo { get; set; }
-        //public string AgenciaDistribuidora { get; set; } = string.Empty;
-        //public AigTramitanteEstudio Tramitante { get; set; } = new AigTramitanteEstudio();
-        //public string? ObservacionTramitante { get; set; }
-
-        //[DefaultValue(EstadoEstudio.Pendiente)]
-        //public EstadoEstudio Estado { get; set; } = EstadoEstudio.Pendiente;
-        //public bool Placebo { get; set; } = false;
-        //public string FrecuenciaImportacion { get; set; } = string.Empty;
-
-        //[IgnoreDataMember]
-        //public AigNotaEstudio Nota { get; set; } = new AigNotaEstudio();
-
-        //[JsonIgnore]
-        //public double ElapsedDays = 0;
-
-        //private ICollection<AigEstudioEvaluador> _estudioEvaluador;
-        //[JsonIgnore]
-        //public virtual ICollection<AigEstudioEvaluador> EstudioEvaluador => _estudioEvaluador ??= new HashSet<AigEstudioEvaluador>();
-
-        //[IgnoreDataMember]
-        //public List<string> Evaluators { get; set; }
-
-        //[IgnoreDataMember]
-        //public string EvaluatorToShow { get; set; }
-
-        //public long? AigEstudioDNFDId { get; set; }
-        //[JsonIgnore]
-        //public virtual AigEstudioDNFD? AigEstudioDNFD { get; set; }
-
-        //[IgnoreDataMember]
-        //public bool Match { get; set; } = true;
-        //[IgnoreDataMember]
-        //public string MatchInfo { get; set; } = string.Empty;
-
-        //private List<AigEstudioFile>? _Documents;
-        //public List<AigEstudioFile>? Documents
-        //{
-        //    get => _Documents ??= new List<AigEstudioFile>();
-        //    set
-        //    {
-
-        //        _Documents = value;
-        //    }
-
-
         public async Task<IResult> Handle(CloneStudyCommand request, CancellationToken cancellationToken)
         {
             IResult answer = new Result();
