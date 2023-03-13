0bd99b3802f00913d888386b4a68926e7f792ffb
igomez <igomez@soaint.com>
2023-03-13 08:38:05 -0500

    update

--- Aig.Auditoria/Controllers/ActasController.cs
+++ Aig.Auditoria/Controllers/ActasController.cs
@@ -1,10 +1,12 @@
 ﻿using Aig.Auditoria.Pages.Inspections;
+using Aig.Auditoria.Services;
 using DataAccess;
 using DataModel;
 using DataModel.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 using static SkiaSharp.HarfBuzz.SKShaper;
 
 namespace Aig.Auditoria.Controllers
@@ -14,9 +16,11 @@ namespace Aig.Auditoria.Controllers
     public class ActasController : ControllerBase
     {
         private readonly IDalService dalService;
-        public ActasController(IDalService dalService)
+        private readonly IPdfGenerationService pdfGenerationService;
+        public ActasController(IDalService dalService,IPdfGenerationService pdfGenerationService)
         {
             this.dalService= dalService;
+            this.pdfGenerationService = pdfGenerationService;
         }
 
         [HttpGet("GetByNumber")]
@@ -35,5 +39,38 @@ namespace Aig.Auditoria.Controllers
             catch(Exception ex) { return BadRequest(new { message = ex.Message }); }
             return BadRequest(new { message = "data not found" });
         }
+
+
+        [HttpGet("CheckByNumber")]
+        public async Task<IActionResult> CheckByNumber(string number) {
+            try {
+                if (!string.IsNullOrEmpty(number)) {
+                    var data = dalService.Find<AUD_InspeccionTB>(x => x.NumActa == number);
+                    if (data != null) {
+                        return Ok(new InspeccionModel() { Nombre=DataModel.Helper.Helper.GetDescription(data.TipoActa), NumeroActa = data.NumActa });
+                    }
+                }
+            }
+            catch (Exception ex) { return BadRequest(new { message = ex.Message }); }
+            return BadRequest(new { message = "data not found" });
+        }
+
+        [HttpGet("GetFileByNumber")]
+        public async Task<IActionResult> GetFileByNumber(string number) {
+            try {
+                if (!string.IsNullOrEmpty(number)) {
+                    var data = dalService.Find<AUD_InspeccionTB>(x => x.NumActa == number);
+                    if (data != null) {
+
+                        var stream = await pdfGenerationService.GenerateInspectionPDF(data.Id);
+                        return File(stream, "application/octet-stream", string.Format("{0}.pdf", DataModel.Helper.Helper.GetDescription(data.TipoActa)));
+
+                    }
+                }
+            }
+            catch (Exception ex) { return BadRequest(new { message = ex.Message }); }
+            return BadRequest(new { message = "datos no encontrados" });
+        }
+
     }
 }
---
+++ DataModel/Models/InspeccionModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel.Models {
+    
+    public class InspeccionModel {
+        public string NumeroActa { get; set; }
+        public string Nombre { get; set; }
+        public string Url { get; set; }
+    }
+}
